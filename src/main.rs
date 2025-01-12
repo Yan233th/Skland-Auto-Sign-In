8962dac291cd27bf0aefc0d9110d0e9ea4508936
Yan233_ <Yan233th@126.com>
2025-01-12 22:55:06 +0800

    Get tokens done.

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::{env, fs, process};
+
+fn main() {
+    // Get user tokens
+    let tokens: Vec<String> = match env::var("USER_TOKENS") {
+        Ok(val) => val.split(';').map(|s| s.trim().to_string()).collect(),
+        Err(_) => {
+            println!("The USER_TOKENS variable was not found in the environment variables, attempting to read from user_tokens.txt.");
+            match fs::read_to_string("user_tokens.txt") {
+                Ok(val) => val.split('\n').map(|s| s.trim().to_string()).collect(),
+                Err(_) => {
+                    eprintln!("Unable to find USER_TOKENS environment variable or user_tokens.txt file!");
+                    process::exit(1);
+                }
+            }
+        }
+    };
+    if tokens.is_empty() {
+        eprintln!("No user tokens found!");
+        process::exit(1);
+    }
+    else {
+        println!("Get user tokens successfully!")
+    }
+    for token in &tokens {
+        println!("'{}'", token);
+    }
+}
